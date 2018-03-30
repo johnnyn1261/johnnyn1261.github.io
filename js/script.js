ed9f0e85d3055b911efc9acf5c80fc52461b9960
johnnyn1261 <johnnyn1261@gmail.com>
2018-03-30 03:48:43 -0400

    Updated navbar action

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//
 $(document).ready(function() {
     setInterval ('cursorAnimation()', 800);
 });
@@ -9,3 +10,21 @@ function cursorAnimation() {
       opacity: 1
   }, 'fast', 'swing');
 }
+
+// Menu-toggle button
+/*$(document).ready(function() {
+      $(".menu-icon").on("click", function() {
+            $("nav ul").toggleClass("showing");
+      });
+});*/
+
+// Scrolling Effect
+$(window).on("scroll", function() {
+      if($(window).scrollTop()) {
+            $('nav').addClass('black');
+      }
+
+      else {
+            $('nav').removeClass('black');
+      }
+})
