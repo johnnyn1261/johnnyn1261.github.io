a2bef6bc5290963a74a2558972a6bff178614994
johnnyn1261 <johnnyn1261@gmail.com>
2018-03-31 01:38:37 -0400

    Added About, Experiences and more assets

--- js/script.js
+++ js/script.js
@@ -11,20 +11,36 @@ function cursorAnimation() {
   }, 'fast', 'swing');
 }
 
+/*
 // Menu-toggle button
-/*$(document).ready(function() {
-      $(".menu-icon").on("click", function() {
-            $("nav ul").toggleClass("showing");
-      });
-});*/
+$(document).ready(function() {
+  $(".navbar-toggler").on("click", function() {
+
+    if ( $('#navbar').is(':visible') ) {
+			$(this).removeClass('active');
+		} else {
+			$(this).addClass('active');
+		}
+
+    $('nav').addClass('black');
+  });
+});
+
+// collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+*/
 
 // Scrolling Effect
 $(window).on("scroll", function() {
-      if($(window).scrollTop()) {
-            $('nav').addClass('black');
-      }
-
-      else {
-            $('nav').removeClass('black');
-      }
+  if($(window).scrollTop() > 100) {
+    $('nav').addClass('black');
+  } else {
+    $('nav').removeClass('black');
+  }
 })
