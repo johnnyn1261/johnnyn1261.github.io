c039348caaa9a77cefa466c74580e8d9fb43bceb
johnnyn1261 <johnnyn1261@gmail.com>
2018-03-31 13:42:52 -0400

    Improved Experiences responsiveness

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ $(window).scroll(function() {
 
 // Scrolling Effect
 $(window).on("scroll", function() {
-  if($(window).scrollTop() > 100) {
+  if($(window).scrollTop() > 20) {
     $('nav').addClass('black');
   } else {
     $('nav').removeClass('black');
