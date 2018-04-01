ce1d6cc6ab08d5fdb1ec1d03f5423b9d50d3698b
johnnyn1261 <johnnyn1261@gmail.com>
2018-04-01 13:28:53 -0400

    Added page scrolling and nav animations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
-//
+
 $(document).ready(function() {
     setInterval ('cursorAnimation()', 800);
+    $(document).on("scroll", onScroll);
+
+    //Scroll annimation
+    $('a[href^="#"]').on('click',function(e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+        $('html, body').animate({
+          'scrollTop': $target.offset().top
+        }, 500,'swing');
+    });
 });
 
 function cursorAnimation() {
@@ -11,8 +22,37 @@ function cursorAnimation() {
   }, 'fast', 'swing');
 }
 
+$('.nav.navbar-nav > li').on('click', function(e) {
+    $('.nav.navbar-nav > li').removeClass('active');
+    $(this).addClass('active');
+});
+
+// Active navbar section
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('#navbarResponsive a').each(function () {
+    var currLink = $(this);
+    var refElement = $(currLink.attr("href"));
+    if (refElement.position().top <= scrollPos + 50) {
+      $('#navbarResponsive ul li a').removeClass("active");
+      currLink.addClass("active");
+    } else{
+      currLink.removeClass("active");
+    }
+  });
+}
+
+// Scrolling effect
+$(window).on("scroll", function() {
+  if($(window).scrollTop() > 20) {
+    $('nav').addClass('black');
+  } else {
+    $('nav').removeClass('black');
+  }
+})
+
 /*
-// Menu-toggle button
+// Menu-toggle automatically sets black background
 $(document).ready(function() {
   $(".navbar-toggler").on("click", function() {
 
@@ -35,12 +75,3 @@ $(window).scroll(function() {
     }
 });
 */
-
-// Scrolling Effect
-$(window).on("scroll", function() {
-  if($(window).scrollTop() > 20) {
-    $('nav').addClass('black');
-  } else {
-    $('nav').removeClass('black');
-  }
-})
